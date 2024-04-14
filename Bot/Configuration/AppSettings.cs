cc1385f3bebdb45d7c7b90adc5f696c48fd96f67
StacyDeveloper <129184938+StacyDeveloper@users.noreply.github.com>
2024-04-14 14:36:42 +0300

    Добавьте файлы проекта.

---
+++ Bot/Configuration/AppSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace VoiceTexterBot.Configuration
+{
+    public class AppSettings
+    {
+        /// Токен Telegram API
+        public string BotToken { get; set; }
+
+        /// Папка загрузки аудио файлов
+        public string DownloadsFolder { get; set; }
+
+        /// Имя файла при загрузке
+        public string AudioFileName { get; set; }
+
+        /// Формат аудио при загрузке
+        public string InputAudioFormat { get; set; }
+
+        /// Формат аудио при загрузке
+        public string OutputAudioFormat { get; set; }
+
+        /// Битрейт аудио при загрузке
+        public float InputAudioBitrate { get; set; }
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VoiceTexterBot.Configuration;
+using VoiceTexterBot.Controllers;
+using VoiceTexterBot.Services;
+
+namespace VoiceTexterBot
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(BuildAppSettings());
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+            services.AddSingleton<IFileHandler, AudioFileHandler>();
+
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient(appSettings.BotToken));
+            services.AddHostedService<Bot>();
+        }
+
+        
+        static AppSettings BuildAppSettings()
+        {
+            return new AppSettings()
+            {
+                DownloadsFolder = "C:\\Users\\anast\\Downloads",
+                BotToken = "6855559272:AAF8T0VlOepFrUDlTOTRaDouC5gzgpJHotQ",
+                AudioFileName = "audio",
+                InputAudioFormat = "ogg",
+                OutputAudioFormat = "wav",
+                InputAudioBitrate = 48000,
+
+            };
+        }
+    }
+}
